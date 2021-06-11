eb68b83ca5b9f52d4e263618e99d98cc0cac1c65
Mingxuan Li <52895636+StrikeR2018@users.noreply.github.com>
2021-06-10 23:19:16 -0700

    Update and rename test_calculator.py to test.py

--- test.py
+++ test.py
@@ -5,19 +5,19 @@ import calculator
 class TestCase(unittest.TestCase):
 
     
-    def test_add_1(self):
+    def test_add(self):
         self.assertEqual(calculator.add(2, 4), 6)
 
         
-    def test_sub_1(self):
+    def test_sub(self):
         self.assertEqual(calculator.subtract(7, 3), 4)
 
         
-    def test_mul_1(self):
+    def test_mul(self):
         self.assertEqual(calculator.mulityply(2, 4), 8)
 
         
-    def test_div_1(self):
+    def test_div(self):
         self.assertEqual(calculator.divide(6, 3), 2)
 
 
